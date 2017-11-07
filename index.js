9cb234ab2d14e310ad79515a2b4b169da3a41a3e
Sean Elwood <seanelwood@gmail.com>
2017-11-07 22:19:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+function scuberGreetingForFeet(totalFeet){
+  if (totalFeet <= 400){
+    return 'This one is on me!';
+  } else if (totalFeet <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+  } else {
+     return "No can do.";
+  }
+}
+
+
+function ternaryCheckCity(city) {
+  (city === 'NYC') ? 'Ok, sounds good.' :
+  'No go.';
+}
