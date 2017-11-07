368a4b14d7ec6ab9ed5b8a39ceb7c22d4271de51
Sean Elwood <seanelwood@gmail.com>
2017-11-07 22:39:55 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+function scuberGreetingForFeet(totalFeet){
+  if (totalFeet <= 400){
+    return 'This one is on me!';
+  } else if (totalFeet <= 2500) {
+      return 'I will gladly take your thirty bucks.';
+  } else {
+     return "No can do.";
+  }
+}
+
+
+function ternaryCheckCity(city) {
+  return (city === 'NYC') ? 'Ok, sounds good.' :
+  'No go.';
+}
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+     return 'Thank you so much.';
+     break;
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
+}
